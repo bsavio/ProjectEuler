8939d0cbff8668e864f6903c022ee360e01d9b99
bsavio <william.savio@gmail.com>
2014-12-08 06:03:52 -0500

    Problem 9 done.  Added Problem 10 & 11

--- Problem9.py
+++ Problem9.py
@@ -8,4 +8,51 @@ For example, 3^2 + 4^2 = 9 + 16 = 25 = 5^2.
 
 There exists exactly one Pythagorean triplet for which a + b + c = 1000.
 Find the product abc.
+
+Parent/child relationships
+By a result of Berggren (1934), all primitive Pythagorean triples can be generated from the (3, 4, 5) triangle by using the three linear transformations T1, T2, T3 below, where a, b, c are sides of a triple:
+
+    new side a     new side b	   new side c
+T1:	a − 2b + 2c    2a − b + 2c    2a − 2b + 3c
+T2:	a + 2b + 2c    2a + b + 2c     2a + 2b + 3c
+T3:	−a + 2b + 2c   −2a + b + 2c	   −2a + 2b + 3c
+
+
+
+from lib import find_pythagorean_triplet
+
+result = find_pythagorean_triplet(3, 4, 5, 1000)
+print(result)
+product = 1
+for i in result:
+    print(i)
+    product *= i
+print(product)
 """
+
+from lib import primitive_pythagorean_triples
+
+for triple in primitive_pythagorean_triples():
+    triple.append(triple[0] + triple[1] + triple[2])
+    if triple[3] >= 1000:
+        break
+    n = 1
+    while triple[3] * n <= 1000:
+        if triple[3] * n == 1000:
+            print('*********************')
+            print('FOUND IT!!')
+            print(n)
+            print(triple)
+            triple[0] *= n
+            triple[1] *= n
+            triple[2] *= n
+            triple[3] = triple[0] + triple[1] + triple[2]
+            triple.append(triple[0] * triple[1] * triple[2])
+            print(triple)
+            break
+        n += 1
+
+
+
+
+
--- lib.py
+++ lib.py
@@ -56,9 +56,51 @@ def is_prime(n):
     """
     if n <= 2 or is_even(n):
         return n == 2
-    for x in range(3,  int(sqrt(n)) + 1, 2):
+    for x in range(3, int(sqrt(n)) + 1, 2):
         if n % x == 0:
             return False
     return True
 
 
+def find_pythagorean_triplet(a, b, c, perimeter):
+    """
+    Finds the the pythagorean triplet for the given perimeter.
+    Pass a = 3, b = 4, c = 5 to start
+    :param a:
+    :type b: int
+    :param b:
+    :type a: int
+    :param c:
+    :type c: int
+    :param perimeter:
+    :type perimeter: int
+    :return:
+    """
+    abc_sum = a + b + c
+    if abc_sum > perimeter:
+        return []
+    if a % 10 == 0 and b % 10 == 0 and c % 10 == 0:
+        print([a, b, c])
+    if abc_sum == perimeter:
+        return [a, b, c]
+    first = find_pythagorean_triplet(a - 2 * b + 2 * c, 2 * a - b + 2 * c, 2 * a - 2 * b + 3 * c, perimeter)
+    if len(first) == 3:
+        return first
+    second = find_pythagorean_triplet(a + 2 * b + 2 * c, 2 * a + b + 2 * c, 2 * a + 2 * b + 3 * c, perimeter)
+    if len(second) == 3:
+        return second
+    third = find_pythagorean_triplet(-a + 2 * b + 2 * c, -2 * a + b + 2 * c, -2 * a + 2 * b + 3 * c, perimeter)
+    if len(third) == 3:
+        return third
+    return []
+
+
+def primitive_pythagorean_triples():
+    """
+    Iterates all primitive Pythagorean Triples
+    """
+    m = 2
+    while True:
+        for n in range(1, m):
+            yield [m**2 - n**2, 2*m*n, m**2 + n**2]
+        m += 1
