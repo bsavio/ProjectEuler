015a710b451a583fdb587a9eed60d7f7dca54e87
bsavio <william.savio@gmail.com>
2015-04-24 06:59:08 -0400

    fixup! Problem 38

--- Problem38.py
+++ Problem38.py
@@ -1,1 +1,47 @@
-__author__ = 'willi_000'
+"""
+Pandigital multiples
+Problem 38
+Take the number 192 and multiply it by each of 1, 2, and 3:
+
+192 × 1 = 192
+192 × 2 = 384
+192 × 3 = 576
+
+By concatenating each product we get the 1 to 9 pandigital, 192384576. We will call 192384576 the concatenated product
+of 192 and (1,2,3)
+
+The same can be achieved by starting with 9 and multiplying by 1, 2, 3, 4, and 5, giving the pandigital, 918273645,
+which is the concatenated product of 9 and (1,2,3,4,5).
+
+What is the largest 1 to 9 pandigital 9-digit number that can be formed as the concatenated product of an integer with
+(1,2, ... , n) where n > 1?
+"""
+
+from timeit import default_timer
+import lib
+
+
+def main():
+    pandigitals = []
+    for i in range(1, 10000):
+        product_digits = []
+        x = 1
+        while len(product_digits) < 9:
+            product_digits = lib.number_to_digits(i * x) + product_digits
+            if 0 in product_digits or len(product_digits) != len(set(product_digits)):
+                break
+            x += 1
+        else:
+            if len(product_digits) == 9 and len(set(product_digits) & {1, 2, 3, 4, 5, 6, 7, 8, 9}) == 9:
+                if i == 192:
+                    print(lib.digits_to_number(product_digits))
+                pandigitals.append(lib.digits_to_number(product_digits))
+    print(pandigitals)
+    print(max(pandigitals))
+    return 0
+
+
+if __name__ == '__main__':
+    start = default_timer()
+    main()
+    print(u"Runtime: {0} ms".format((default_timer() - start) * 1000))
